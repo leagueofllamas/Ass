b7e606db1e830cea5f2c8aa465c8c857c324a893
leagueofllamas <lutzd55@icloud.com>
2017-05-03 21:00:35 -0400

    Lol dank memes

--- src/DankMemesRCool.java
+++ src/DankMemesRCool.java
@@ -4,5 +4,14 @@
 public class DankMemesRCool {
     public static void main(String[] args) {
         System.out.println("Hello darkness my old friend");
+        sand("Danks",69);
+    }
+    public static void sand(String apple, int daniel){
+
+        int sandwhich=0;
+        while (!(sandwhich==daniel)) {
+            sandwhich++;
+            System.out.println(apple+sandwhich);
+        }
     }
 }
